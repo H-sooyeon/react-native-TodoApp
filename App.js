9a824acc224cc0bea805f793c44b788e29f80d20
sooyeon <wndqhr__@naver.com>
2023-03-28 21:54:41 +0900

    토글및 일정 추가 기능

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  View,
-  Text,
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
@@ -11,9 +9,35 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 function App() {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
+  const onInsert = text => {
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
 
   return (
     <SafeAreaProvider>
@@ -22,8 +46,12 @@ function App() {
           behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
-          <AddTodo />
+          {todos.length === 0 ? (
+            <Empty />
+          ) : (
+            <TodoList todos={todos} onToggle={onToggle} />
+          )}
+          <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
